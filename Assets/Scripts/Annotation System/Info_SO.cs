8b0b0632d8757f2c696de85d7d20d62d2b7ea5e1
ILunaria <XxantxX752@gmail.com>
2023-05-11 21:19:16 -0400

    Provavelmente Quebrado

--- Assets/Scripts/Annotation System/Info_SO.cs
+++ Assets/Scripts/Annotation System/Info_SO.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Assets/Info",fileName = "Info_")]
 public class Info_SO : ScriptableObject
 {
+    [SerializeField] private string title;
     [TextArea(2, 10)]
     [SerializeField] private string text;
     [SerializeField] private string index;
     [SerializeField] private InfoType type;
 
+    public string Title => title;
     public string Text => text;
     public string Index => index;
     public InfoType Type => type;
